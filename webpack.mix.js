e37be1f545b01f3f7dd692bd22b9a8182747e08d
temo-123 <samsonadze.temo9@gmail.com>
2023-05-22 18:05:57 +0000

    test: Testing host bugs 2

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@
 const mix = require('laravel-mix');
 require('laravel-vue-lang/mix');
 
-require('dotenv').config({path:'../../.env'}); // laravel-mix for using .env varibles in VUE Components
+require('dotenv').config({path:'.env'}); // laravel-mix for using .env varibles in VUE Components
 
 mix.js('resources/js/app.js', 'public/assets/js').vue();
 mix.sass('resources/sass/app.scss', 'public/assets/css', []);
