453cacd4d6b5fd7ae16d8eb836f5b467f24cab65
temo-123 <samsonadze.temo9@gmail.com>
2024-04-22 05:22:51 +0000

    fix: Fixed SpotRockController errors

--- app/Http/Controllers/Api/Guide/SpotRockController.php
+++ app/Http/Controllers/Api/Guide/SpotRockController.php
@@ -7,16 +7,12 @@ use Illuminate\Http\Request;
 
 use App\Models\Guide\Spot_rocks_image;
 
-use App\Services\ImageControllService;
+use App\Services\Abstract\ImageControllService;
 
 class SpotRockController extends Controller
 {
     public function get_spot_rock_images(Request $request)
     {
-        // $image = Spot_rocks_image::where('id', '=', $request->image_id)->first();
-        // ImageControllService::image_delete('images/spot_rock_img/', $image, 'image');
-        // $image ->delete();
-
         return Spot_rocks_image::where('article_id', '=', $request->article_id)->get();
     }
 
