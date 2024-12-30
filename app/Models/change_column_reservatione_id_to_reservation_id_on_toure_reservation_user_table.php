6b8debd992a78954042a2eab8c7bea5578bd4635
temo-123 <samsonadze.temo9@gmail.com>
2024-12-30 18:50:51 +0000

    fix: Fixed tour reservation error

---
+++ app/Models/change_column_reservatione_id_to_reservation_id_on_toure_reservation_user_table.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class change_column_reservatione_id_to_reservation_id_on_toure_reservation_user_table extends Model
+{
+    use HasFactory;
+}
