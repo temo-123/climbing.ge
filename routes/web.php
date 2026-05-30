e5021be6ad2c89eb0237cdcf373979b6f58bc92e
temo-123 <samsonadze.temo9@gmail.com>
2026-05-30 17:38:09 +0000

    fix: fixed routes error

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ if (!function_exists('spaError')) {
     }
 }
 
-    Route::domain(config('app.url'))->group(function () {
+    Route::domain(env('SITE_URL'))->group(function () {
         Route::get('/404',    fn() => spaError('site.index', 404));
         Route::get('/403',    fn() => spaError('site.index', 403));
         Route::get('/500',    fn() => spaError('site.index', 500));
@@ -22,7 +22,7 @@ if (!function_exists('spaError')) {
         });
     });
 
-    Route::domain('films.' . config('app.url'))->group(function () {
+    Route::domain(env('FILMS_URL'))->group(function () {
         Route::get('/404',    fn() => spaError('films.index', 404));
         Route::get('/403',    fn() => spaError('films.index', 403));
         Route::get('/500',    fn() => spaError('films.index', 500));
@@ -32,7 +32,7 @@ if (!function_exists('spaError')) {
         });
     });
 
-    Route::domain('shop.' . config('app.url'))->group(function () {
+    Route::domain(env('SHOP_URL'))->group(function () {
         Route::get('/404',    fn() => spaError('shop.index', 404));
         Route::get('/403',    fn() => spaError('shop.index', 403));
         Route::get('/500',    fn() => spaError('shop.index', 500));
@@ -42,7 +42,7 @@ if (!function_exists('spaError')) {
         });
     });
 
-    Route::domain('blog.' . config('app.url'))->group(function () {
+    Route::domain(env('BLOG_URL'))->group(function () {
         Route::get('/404',    fn() => spaError('blog.index', 404));
         Route::get('/403',    fn() => spaError('blog.index', 403));
         Route::get('/500',    fn() => spaError('blog.index', 500));
@@ -52,7 +52,7 @@ if (!function_exists('spaError')) {
         });
     });
 
-    Route::domain('summit.' . config('app.url'))->group(function () {
+    Route::domain(env('SUMMIT_URL'))->group(function () {
         Route::get('/404',    fn() => spaError('summit.index', 404));
         Route::get('/403',    fn() => spaError('summit.index', 403));
         Route::get('/500',    fn() => spaError('summit.index', 500));
@@ -62,7 +62,7 @@ if (!function_exists('spaError')) {
         });
     });
 
-    Route::domain('user.' . config('app.url'))->group(function () {
+    Route::domain(env('USER_PAGE_URL'))->group(function () {
         Route::get('/404',    fn() => spaError('user.home', 404));
         Route::get('/403',    fn() => spaError('user.home', 403));
         Route::get('/500',    fn() => spaError('user.home', 500));
